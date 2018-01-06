c828f887c41b84b81bfeb5e8a9cdb545f57e9598
nerdale <alexandra.neira@gmail.com>
2018-01-05 23:19:56 -0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,96 @@
+$(document).ready(() => {
+});
+/*
+* Si analizan la data, se darán cuenta que en la variable data podemos encontrar
+* 4 objetos, estos son: gossip, breakingbad, howtoget, stranger.
+* Cada objeto en su interior, tiene el objeto characters, que a su vez está compuesto
+* por un arreglo de objetos donde se almacen los datos de cada uno de los personajes
+* Pueden visualizar esto haciendo los siguientes console.log
+* console.log(data);
+* console.log(data.gossip.characters);
+* console.log(data.breakingbad.characters);
+* console.log(data.howtoget.characters);
+* console.log(data.stranger.characters);
+*/
+
+/*
+* Genero las variables donde guardaré el acceso a characters de cada uno de los objetos (series)
+* ¿por qué hago esto? para que cuando lo utilice en el for no me maree tanto texto :)
+* Es visualmente más ordenado y entendible
+*/
+var gossip = data.gossip.characters;
+var breaking = data.breakingbad.characters;
+var howtoget = data.howtoget.characters;
+
+/*
+* Ahora comenzamos con el contenido. Cuando se le haga click al botón de Gossip girl, mostraremos
+* sus personajes desde jquery y la data proporcionada.
+* Para eso creamos el evento de click en el botón que queremos y le decimos que una vez ejecutado
+* este evento recorra el largo del arreglo donde están almacenados los datos de los personajes de
+* gossip girl (data.gossip.characters) el que ya tenemos guardado en la variable gossip.
+*/
+$('.btn-gossip').click(function(){
+  for (var i = 0; i < gossip.length; i++) {
+    /*
+    * Lo que hacemos acá es que como se repite lo mismo en cada botón por cada serie (los otros eventos
+    * que están más abajo) le pedimos que borre todo el contenido generado en los otros div y solo
+    * queden insertados los del evento ejecutado.
+    * Si presiono primero breaking bad, se cargan sus personajes, pero luego presiono gossip girl
+    * se borra inmediatamente todo el contenido de breaking bad y se genera el de gossip girl
+    * cada uno en su div correspondiente
+    */
+    $('.breaking, .how').children().remove(); //a los divs con esas clases les remueve todos sus hijos
+    /*
+    * Le indicamos que en cada iteración, es decir cuando pase por cada personaje, genera la
+    * siguiente estructura por cada uno, sacando así el dato de nombre, colegio, ciudad y foto,
+    * haciendo el respectivo append a su div contenedor que está vacío en el html (<div class="gossip"></div>)
+    */
+    $('.gossip').append('<div class="row character">' +
+                          '<div class= "col-md-6 text-center">'+
+                            '<img src="assets/' + gossip[i].picture + '">' +
+                          '</div>' +
+                          '<div class= "col-md-6">'+
+                            '<h3>' + gossip[i].name + '</h3>' +
+                            '<p>School: ' + gossip[i].school + '</p>' +
+                            '<p>City: ' + gossip[i].city + '</p>' +
+                            '</div>' +
+                          '</div>')
+  }
+})
+
+/*
+* Si se dan cuenta en los siguientes eventos repetimos exactamente lo mismo
+* Si al hacer click en cada botón inspeccionan el html, se darán cuenta que el contenido
+* se borra y agrega desde jquery, nunca ha estado en el html, evitando así recargarlo
+* de contenido y simular que no está visible utilizando hide() y show()
+*/
+
+$('.btn-breaking').click(function(){
+  for (var i = 0; i < breaking.length; i++) {
+    $('.gossip, .how').children().remove();
+    $('.breaking').append('<div class="row character">' +
+                          '<div class= "col-md-6 text-center">'+
+                            '<img src="assets/' + breaking[i].picture + '">' +
+                          '</div>' +
+                          '<div class= "col-md-6">'+
+                            '<h3>' + breaking[i].name + '</h3>' +
+                            '<p>City: ' + breaking[i].city + '</p>' +
+                            '</div>' +
+                          '</div>')
+  }
+})
+
+$('.btn-how').click(function(){
+  for (var i = 0; i < howtoget.length; i++) {
+    $('.gossip, .breaking').children().remove();
+    $('.how').append('<div class="row character">' +
+                          '<div class= "col-md-6 text-center">'+
+                            '<img src="assets/' + howtoget[i].picture + '">' +
+                          '</div>' +
+                          '<div class= "col-md-6">'+
+                            '<h3>' + howtoget[i].name + '</h3>' +
+                            '<p>City: ' + howtoget[i].city + '</p>' +
+                            '</div>' +
+                          '</div>')
+  }
+})
---
+++ assets/js/data.js
@@ -0,0 +1,150 @@
+
+var data = {
+  'gossip': {
+    'characters': [
+      {
+        'name': 'Blair Waldorf',
+        'picture': 'img/gossip/blair.jpg',
+        'school': 'Constance Billard School',
+        'city': 'Manhattan, New York'
+      },
+      {
+        'name': 'Serena Van Der Woodsen',
+        'picture': 'img/gossip/serena.jpg',
+        'school': 'Constance Billard School',
+        'city': 'Manhattan, New York'
+      },
+      {
+        'name': 'Jenny Humphrey',
+        'picture': 'img/gossip/jenny.jpg',
+        'school': 'Constance Billard School',
+        'city': 'Brooklyn, New York'
+      },
+      {
+        'name': 'Chuck Bass',
+        'picture': 'img/gossip/chuck.jpg',
+        'school': 'Riverside Preparatory School',
+        'city': 'Manhattan, New York'
+      },
+      {
+        'name': 'Nate Archibald',
+        'picture': 'img/gossip/nate.jpg',
+        'school': 'Riverside Preparatory School',
+        'city': 'Manhattan, New York'
+      },
+      {
+        'name': 'Dan Humphrey',
+        'picture': 'img/gossip/dan.jpg',
+        'school': 'Riverside Preparatory School',
+        'city': 'Brooklyn, New York'
+      }
+    ]
+  },
+  'breakingbad': {
+    'characters': [
+      {
+        'name': 'Walter White',
+        'picture': 'img/breakingbad/walter.jpg',
+        'city': 'Albuquerque'
+      },
+      {
+        'name': 'Skyler White',
+        'picture': 'img/breakingbad/skyler.jpg',
+        'city': 'Albuquerque'
+      },
+      {
+        'name': 'Jesse Pinkman',
+        'picture': 'img/breakingbad/jesse.jpg',
+        'city': 'Albuquerque'
+      },
+      {
+        'name': 'Gus Fring',
+        'picture': 'img/breakingbad/gus.jpg',
+        'city': 'Albuquerque'
+      },
+      {
+        'name': 'Mike Ehrmantraut',
+        'picture': 'img/breakingbad/mike.jpg',
+        'city': 'Albuquerque'
+      }
+    ]
+  },
+  'howtoget': {
+    'characters': [
+      {
+        'name': 'Annalise Keating',
+        'picture': 'img/howtoget/annalise.jpg',
+        'city': 'Philadelphia'
+      },
+      {
+        'name': 'Connor Walsh',
+        'picture': 'img/howtoget/connor.png',
+        'city': 'Philadelphia'
+      },
+      {
+        'name': 'Wes Gibbins',
+        'picture': 'img/howtoget/wes.jpg',
+        'city': 'Philadelphia'
+      },
+      {
+        'name': 'Asher Millstone',
+        'picture': 'img/howtoget/asher.jpg',
+        'city': 'Philadelphia'
+      },
+      {
+        'name': 'Michaela Pratt',
+        'picture': 'img/howtoget/michaela.jpg',
+        'city': 'Philadelphia'
+      },
+      {
+        'name': 'Frank Delfino',
+        'picture': 'img/howtoget/frank.jpg',
+        'city': 'Philadelphia'
+      },
+      {
+        'name': 'Bonnie Winterbottom',
+        'picture': 'img/howtoget/bonnie.jpg',
+        'city': 'Philadelphia'
+      }
+    ]
+  },
+  'stranger': {
+    'characters': [
+      {
+        'name': 'Eleven',
+        'picture': 'img/stranger/eleven.jpg',
+        'city': 'Hawkins, Indiana'
+      },
+      {
+        'name': 'Mike Wheeler',
+        'picture': 'img/stranger/mike.jpg',
+        'city': 'Hawkins, Indiana'
+      },
+      {
+        'name': 'Dustin Henderson',
+        'picture': 'img/stranger/dustin.jpg',
+        'city': 'Hawkins, Indiana'
+      },
+      {
+        'name': 'Lucas Sinclair',
+        'picture': 'img/stranger/lucas.jpg',
+        'city': 'Hawkins, Indiana'
+      },
+      {
+        'name': 'Will Byers',
+        'picture': 'img/stranger/will.jpg',
+        'city': 'Hawkins, Indiana'
+      },
+      {
+        'name': 'Nancy Wheeler',
+        'picture': 'img/stranger/nancy.png',
+        'city': 'Hawkins, Indiana'
+      },
+      {
+        'name': 'Joyce Byers',
+        'picture': 'img/stranger/joyce.jpg',
+        'city': 'Hawkins, Indiana'
+      }
+    ]
+  }
+}
